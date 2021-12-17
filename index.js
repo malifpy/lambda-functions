64a3320f29b72de4dd8af2c5985bb5d3ead7a50c
Muhammad Alif Putra Yasa <malifputrayasa@gmail.com>
2021-12-17 07:32:46 +0700

    Test

---
+++ index.js
@@ -0,0 +1,21 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    };
+    
+    const params = JSON.parse(event.body);
+    
+    response.body = JSON.stringify(params);
+    
+    
+    /*
+    const params = event.queryStringParameters
+    if (params == null) {
+        response.body = JSON.stringify("No Parameters");
+    } else {
+        response.body = JSON.stringify(params);
+    }
+    */
+    return response;
+};
