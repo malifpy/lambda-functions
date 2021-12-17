e86e57f0cef730298f042713229fd53f9f1b2f2d
Muhammad Alif Putra Yasa <malifputrayasa@gmail.com>
2021-12-17 18:14:10 +0700

    Added try and catch

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const exactMath = require('exact-math');
+const exactMath = require("exact-math");
 
 function calc(formula) {
-    return exactMath.formula(formula);
+  return exactMath.formula(formula);
 }
 
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    };
-    
-    const params = JSON.parse(event.body);
-    
-    if (params.func === "Calculator") {
-        response.body = JSON.stringify(calc(params.formula));
-    } else {
-        response.body = JSON.stringify("No Input");
+  // TODO implement
+  const response = {
+    statusCode: 200,
+  };
+
+  const params = JSON.parse(event.body);
+
+  if (params.func === "Calculator") {
+    try {
+      response.body = JSON.stringify(calc(params.formula));
+    } catch {
+      response.body = JSON.stringify("Output error: " + params.formula);
     }
-    
-    
-    /*
+  } else {
+    response.body = JSON.stringify("No Input");
+  }
+
+  /*
     const params = event.queryStringParameters
     if (params == null) {
         response.body = JSON.stringify("No Parameters");
@@ -27,5 +30,5 @@ exports.handler = async (event) => {
         response.body = JSON.stringify(params);
     }
     */
-    return response;
+  return response;
 };
